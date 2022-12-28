4f82f2df58bdedf19a0fdaa87e2f8d5e391b0881
Weslley Paes <95313382+Weslleypaes@users.noreply.github.com>
2022-12-28 11:50:25 -0300

    Add files via upload

---
+++ notas/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+
+    double nota1, nota2, notaFinal;
+
+    printf("Digite a primeira nota: ");
+    scanf("%lf", & nota1);
+
+    printf("Digite a segunda nota: ");
+    scanf("%lf", & nota2);
+
+    notaFinal = nota1 + nota2;
+
+    if(notaFinal < 60 ){
+        printf("NOTA FINAL = %.1lf\n", notaFinal );
+        printf("REPROVADO !");
+    } else {
+        printf("NOTA FINAL = %.1lf", notaFinal );
+    };
+
+    return 0;
+}
